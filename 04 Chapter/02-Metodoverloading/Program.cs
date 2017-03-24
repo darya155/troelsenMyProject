8cf3c7f52db63949748f6d18632a01dad77aa4a3
darya155 <fiola@mail.ru>
2017-03-24 09:59:20 +0300

    создан новый проект 02-Metodoverloading, где рассмотрена перегрузка метода

---
+++ 04 Chapter/02-Metodoverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Metodoverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Metod Overloading *****\n");
+            //вызов int-версии Add()
+            Console.WriteLine(Add(10, 10));
+            //вызов long-версии Add()
+            Console.WriteLine(Add(900000000, 900000000));
+            //вызов double-версии Add()
+            Console.WriteLine(Add(4.3, 4.4));
+        }
+        //перегруженный метод Add()
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        static long Add(long x, long y)
+        {
+            return x + y;
+        }
+    }
+}
