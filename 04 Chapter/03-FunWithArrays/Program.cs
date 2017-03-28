ee38e6593559722554485a90352abf006e6a92dc
darya155 <fiola@mail.ru>
2017-03-28 11:49:20 +0300

    неявно типизированные массивы var

--- 04 Chapter/03-FunWithArrays/Program.cs
+++ 04 Chapter/03-FunWithArrays/Program.cs
@@ -13,6 +13,7 @@ namespace _03_FunWithArrays
             Console.WriteLine("***** Fun with Arrays *****");
             SimpleArrays();
             ArrayInitialization();
+            DeclareImplisitArrays();
         }
 
         #region создание и заполнение простого массива
@@ -59,6 +60,24 @@ namespace _03_FunWithArrays
             Console.WriteLine("intArray has {0} elements", intArray.Length);
             Console.WriteLine();
         }
+        #endregion
+
+        #region неявно типизированные локальные массивы
+        static void DeclareImplisitArrays()
+        {
+            Console.WriteLine("=> Implisit Arrray Initialisation.");
+            //а - на самом деле int[]
+            var a = new[] { 1, 10, 100, 1000 };
+            Console.WriteLine("a is a: {0}", a.ToString());
+            //b - на самом деле double[]
+            var b = new[] { 1, 1.5, 2, 2.5 };
+            Console.WriteLine("b is a: {0}", b.ToString());
+            //с - на самом деле string[]
+            var c = new[] { "hello", null, "world" };
+            Console.WriteLine("c is a: {0}", c.ToString());
+            Console.WriteLine();
+        }
+
         #endregion
     }
 }
